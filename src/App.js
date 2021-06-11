0515e088c4755f5e951ff732722edf8df9f493ed
Paul Schlattmann <paulschlattmann@Pauls-MacBook-Pro.local>
2021-06-11 16:57:42 -0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
+import React, {useEffect, useState, setIsLoaded, setItems, setError} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("https://api.example.com/items")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            {item.name} {item.price}
+          </li>
+        ))}
+      </ul>
+    );
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -21,5 +62,6 @@ function App() {
     </div>
   );
 }
+  
 
 export default App;
